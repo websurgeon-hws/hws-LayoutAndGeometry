4ed8d98a21df02e26e4fa4f54095477a6bb7ef89
Peter Barclay <peter@peter-barclay.com>
2019-12-24 04:22:50 +0000

    Alignment and alignment guides - alignmentGuide

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -6,15 +6,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .lastTextBaseline) {
-            Text("Live")
-                .font(.caption)
-            Text("long")
-            Text("and")
-                .font(.title)
-            Text("prosper")
-                .font(.largeTitle)
+        VStack(alignment: .leading) {
+            ForEach(0..<10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { _ in
+                        CGFloat(position) * -10
+                }
+            }
         }
+        .background(Color.red)
+        .frame(width: 400, height: 400)
+        .background(Color.blue)
     }
 }
 
