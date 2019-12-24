e00acf1f8a2a3ed6f4b3ccd26b2e2daad20e589f
Peter Barclay <peter@peter-barclay.com>
2019-12-24 04:18:08 +0000

    Alignment and alignment guides - lastTextBaseline

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -6,9 +6,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Live long and prosper")
-            .offset(x: 50, y: 50)
-            .frame(width: 300, height: 300, alignment: .topLeading)
+        HStack(alignment: .lastTextBaseline) {
+            Text("Live")
+                .font(.caption)
+            Text("long")
+            Text("and")
+                .font(.title)
+            Text("prosper")
+                .font(.largeTitle)
+        }
     }
 }
 
